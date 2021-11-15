c668fdda24a6e6529bfcca773d4c21fa6f4ffe99
Peter Kota <peterkota@kinsta.com>
2021-11-15 15:10:06 +0100

    fix db

--- knexfile.ts
+++ knexfile.ts
@@ -2,23 +2,25 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-if (
-  !process.env.DB_HOST ||
-  !process.env.DB_PORT ||
-  !process.env.DB_USER ||
-  !process.env.DB_NAME
-) {
-  throw new Error('Please configure the database environment variables first!')
-}
+// if (
+//   !process.env.DB_HOST ||
+//   !process.env.DB_PORT ||
+//   !process.env.DB_USER ||
+//   !process.env.DB_NAME
+// ) {
+//   throw new Error('Please configure the database environment variables first!')
+// }
 
 export = {
   client: 'mysql',
   connection: {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    connectionString: process.env.DB_URL,
+    ssl: { rejectUnauthorized: false },
+    // host: process.env.DB_HOST,
+    // port: process.env.DB_PORT,
+    // user: process.env.DB_USER,
+    // password: process.env.DB_PASSWORD,
+    // database: process.env.DB_NAME,
   },
   seeds: {
     directory: './src/db/seeds',
